88a1083bdb5e46c015c808dc2fbe6f31f093d347
mrzahidjabbar <mrzahidjabbar@gmail.com>
2021-06-15 03:36:35 +0500

    Fix: eslint issue

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
   useEffect(() => {
     handleSeacrch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage]);
 
   const handleLoginChange = (e) => {
